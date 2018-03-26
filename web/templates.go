b8d7dc9db1018907f84007742da709af19953fd0
duanshuaimin <duanshuaimin@hotmail.com>
2018-03-26 20:54:27 +0800

    Update templates.go

--- web/templates.go
+++ web/templates.go
@@ -71,7 +71,7 @@ var TemplateText = map[string]string{
   `,
 
 	"jsdep": `
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 `,
 
